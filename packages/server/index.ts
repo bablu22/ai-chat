d8aa141acb41c7c60590b20f8856e5364b73c800
bablu22 <mdbablu22.bablu22@gmail.com>
2025-09-19 07:32:35 +0600

    feat: server initial setup

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
