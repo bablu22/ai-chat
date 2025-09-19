06da2df4284db20ba5a39663720a7f7805181437
bablu22 <mdbablu22.bablu22@gmail.com>
2025-09-19 07:57:30 +0600

    feat: add prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 const App = () => {
-  return (
-    <div>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
+   return (
+      <div>
+         <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default App;
